4ab5e47125271f48355228ec951358dc1430dbd2
bugsbrog <bugsbrog@gmail.com>
2022-02-09 19:22:40 -0700

    Added more stuff to index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    res.json([
+        { id: 1, username: 'Hannah' },
+        { id: 2, username: 'Trey' },
+        { id: 3, username: 'Philip' },
+        { id: 4, username: 'Danielle' },
+        { id: 5, username: 'Ryan' },
+    ])
+})
+
 const PORT = process.env.PORT || 9000
 
 server.listen(PORT, () => {
